0d12e0ac96fe10c33729919c47cd538a30d8b233
LeviMonsha <lev.coposow@yandex.ru>
2024-03-31 17:08:01 +0300

    Adding calculator logic

--- Calculator/src/Resourses/KeyHandler.java
+++ Calculator/src/Resourses/KeyHandler.java
@@ -1,7 +1,40 @@
-package Resourses;
-
-
-public class KeyHandler {
-
-
-}
+//package Resourses;
+//
+//import java.awt.event.KeyEvent;
+//import java.awt.event.KeyListener;
+//
+//public class KeyHandler implements KeyListener {
+//    private StringBuilder textArea;
+//    private static final int ESC = 27;
+//    public static boolean running = true;
+//
+//    @Override
+//    public void keyTyped(KeyEvent e) {
+//        char keyChar = e.getKeyChar();
+//
+//        char c = e.getKeyChar();
+//        if (Character.isDigit(c)) {
+//            textArea.append(String.valueOf(c));
+//        }
+//
+//        switch (keyChar) {
+//            case '+' -> ;
+//            case '-' -> ;
+//            case '*' -> ;
+//            case '/' -> ;
+//            case '=' -> ;
+//            case ESC -> running = false;
+//            default: break;
+//        }
+//    }
+//
+//    @Override
+//    public void keyPressed(KeyEvent e) {
+//
+//    }
+//
+//    @Override
+//    public void keyReleased(KeyEvent e) {
+//
+//    }
+//}
